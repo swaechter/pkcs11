9b7c24347dae87f0d62e74212c069293dd46f00b
Simon Wächter <waechter.simon@gmail.com>
2024-07-11 23:38:14 +0200

    Update all dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,16 +11,16 @@ dependencyResolutionManagement {
             plugin("shadow", "com.github.johnrengelman.shadow").version("8.1.1")
 
             // Testing (General)
-            library("junit-api", "org.junit.jupiter:junit-jupiter-api:5.10.2")
-            library("junit-engine", "org.junit.jupiter:junit-jupiter-engine:5.10.2")
+            library("junit-api", "org.junit.jupiter:junit-jupiter-api:5.10.3")
+            library("junit-engine", "org.junit.jupiter:junit-jupiter-engine:5.10.3")
 
             // Testing (PDF signing)
-            library("slf4j-nop", "org.slf4j:slf4j-nop:2.0.12")
-            library("itext-kernel", "com.itextpdf:kernel:8.0.3")
-            library("itext-io", "com.itextpdf:io:8.0.3")
-            library("itext-layout", "com.itextpdf:layout:8.0.3")
-            library("itext-sign", "com.itextpdf:sign:8.0.3")
-            library("itext-bouncycastle", "com.itextpdf:bouncy-castle-adapter:8.0.3")
+            library("slf4j-nop", "org.slf4j:slf4j-nop:2.0.13")
+            library("itext-kernel", "com.itextpdf:kernel:8.0.4")
+            library("itext-io", "com.itextpdf:io:8.0.4")
+            library("itext-layout", "com.itextpdf:layout:8.0.4")
+            library("itext-sign", "com.itextpdf:sign:8.0.4")
+            library("itext-bouncycastle", "com.itextpdf:bouncy-castle-adapter:8.0.4")
         }
     }
 }
